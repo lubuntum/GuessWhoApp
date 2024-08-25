12e91399f7151b5f8c7bf279f5cdae8965d9e92a
lubuntum <lubuntum@gmail.com>
2024-08-25 21:43:50 +0700

    Реализован функционал карточки, добавлена возможность вести записи

--- app/src/main/java/com/lubuntum/guesswhoapp/CharacterCardFragment.java
+++ app/src/main/java/com/lubuntum/guesswhoapp/CharacterCardFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -15,12 +16,6 @@ import com.lubuntum.guesswhoapp.cards.CardLoader;
 import com.lubuntum.guesswhoapp.databinding.FragmentCharacterCardBinding;
 
 import java.util.Random;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CharacterCardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CharacterCardFragment extends Fragment {
     FragmentCharacterCardBinding binding;
     private static int score = 0;
@@ -46,14 +41,42 @@ public class CharacterCardFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        nextCardBtn();
+        scoreBtn();
+        showListBtn();
+    }
+    private void nextCardBtn(){
         binding.nextImageBtn.setOnClickListener(v -> {
+            binding.guessBlank.setText("");
             getNextCard();
         });
+    }
+    private void scoreBtn(){
         binding.addScoreBtn.setOnClickListener(v -> {
             score++;
             binding.addScoreBtn.setText(String.format(getString(R.string.score), score));
         });
         binding.addScoreBtn.setText(String.format(getString(R.string.score), score));
+    }
+
+    private void showListBtn(){
+        binding.showListBtn.setOnClickListener(v -> {
+            if(binding.card.getVisibility() == View.VISIBLE){
+                binding.card.setVisibility(View.GONE);
+                binding.emptyCard.setVisibility(View.VISIBLE);
+                binding.showListBtn.setImageDrawable(
+                        AppCompatResources.getDrawable(
+                                getContext(),
+                                R.drawable.baseline_visibility_24));
+            } else {
+                binding.card.setVisibility(View.VISIBLE);
+                binding.emptyCard.setVisibility(View.GONE);
+                binding.showListBtn.setImageDrawable(
+                        AppCompatResources.getDrawable(
+                                getContext(),
+                                R.drawable.baseline_visibility_off_24));
+            }
+        });
     }
 
     private void getNextCard(){

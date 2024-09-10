c1e29851fec36d87845b8db60467208d8659bb2c
lubuntum <lubuntum@gmail.com>
2024-09-10 19:11:09 +0700

    Класс для работы с локальными данными игрока

---
+++ app/src/main/java/com/lubuntum/guesswhoapp/gamesession/SessionStorage.java
@@ -0,0 +1,7 @@
+package com.lubuntum.guesswhoapp.gamesession;
+/*
+* Класс для хранение (локально) данных о текущем пользователе
+* */
+public class SessionStorage {
+    public static final String CURRENT_USER = "user_data";
+}

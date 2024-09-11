a8c5e5805999fe92371b6df4733d9657cfeefcd5
lubuntum <lubuntum@gmail.com>
2024-09-11 18:26:51 +0700

    MVVM для работа с сессией

---
+++ app/src/main/java/com/lubuntum/guesswhoapp/gamesession/viewmodels/SessionViewModel.java
@@ -0,0 +1,12 @@
+package com.lubuntum.guesswhoapp.gamesession.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class SessionViewModel extends AndroidViewModel {
+    public SessionViewModel(@NonNull Application application) {
+        super(application);
+    }
+}

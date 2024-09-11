3a833faf86d1589dbf6d97206fc66d4d82d87ec9
lubuntum <lubuntum@gmail.com>
2024-09-11 18:27:09 +0700

    Тест для ключа

--- app/src/test/java/com/lubuntum/guesswhoapp/ExampleUnitTest.java
+++ app/src/test/java/com/lubuntum/guesswhoapp/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.lubuntum.guesswhoapp.utils.SessionKeyGenerator;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +15,11 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void testKeyGenerator(){
+        String key = SessionKeyGenerator.generateStr(7);
+        System.out.println(key);
+        assertEquals(7, key.length());
     }
 }

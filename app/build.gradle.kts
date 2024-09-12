47c88f35f5a6804872ff49e18e0d9ceb6abd4a91
lubuntum <lubuntum@gmail.com>
2024-09-12 18:24:12 +0700

    Интеграция сервиса Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -44,10 +45,12 @@ dependencies {
 
     implementation ("com.github.bumptech.glide:okhttp3-integration:4.12.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
+    implementation("com.google.firebase:firebase-database")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")

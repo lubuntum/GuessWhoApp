c4b7b3237e1ba6868fa1674b24c6a26c5f5e99d2
lubuntum <lubuntum@gmail.com>
2024-09-21 09:34:13 +0700

    Регистрация пользователя

--- app/src/main/java/com/lubuntum/guesswhoapp/auth/AuthDialog.java
+++ app/src/main/java/com/lubuntum/guesswhoapp/auth/AuthDialog.java
@@ -5,12 +5,20 @@ import android.renderscript.ScriptGroup;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.lubuntum.guesswhoapp.databinding.AuthDialogBinding;
+import com.lubuntum.guesswhoapp.gamesession.entity.User;
+import com.lubuntum.guesswhoapp.gamesession.viewmodels.SessionViewModel;
+import com.lubuntum.guesswhoapp.utils.DialogAdapt;
 
 public class AuthDialog extends DialogFragment {
     AuthDialogBinding binding;
@@ -24,5 +32,41 @@ public class AuthDialog extends DialogFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        DialogAdapt.adaptByScreenSize(getDialog().getWindow(), getContext(), 0.9);
+        authBtnInit();
+    }
+    private void authBtnInit(){
+        binding.authUserBtn.setOnClickListener(view -> {
+            binding.loadProgress.setVisibility(View.VISIBLE);
+            saveAuthUser();
+        });
+    }
+    private void saveAuthUser(){
+        FirebaseDatabase firebase = FirebaseDatabase.getInstance(SessionViewModel.FIREBASE_PATH);
+        DatabaseReference userRef = firebase.getReference(SessionViewModel.USERS_PATH);
+        User user;
+        if (AuthStorage.isUserExists(getContext())){
+            user = AuthStorage.getCurrentUser(getContext());
+        }
+        else{
+            user = new User();
+            user.id = userRef.push().getKey();
+        }
+        user.name = binding.username.getText().toString();
+        userRef.child(user.id).setValue(user)
+                .addOnSuccessListener(unused -> {
+                    Toast.makeText(
+                            getContext(),
+                            "Регистрация успешна",
+                            Toast.LENGTH_SHORT).show();
+                    binding.loadProgress.setVisibility(View.GONE);
+                    AuthStorage.saveCurrentUser(getContext(), user);
+                    dismiss();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(getContext(),
+                            "Неизвестная ошибка, попробуйте позже",
+                            Toast.LENGTH_SHORT).show();
+                });
     }
 }

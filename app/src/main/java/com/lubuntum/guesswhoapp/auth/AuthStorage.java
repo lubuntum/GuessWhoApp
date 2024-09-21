489bf066ed72ea43643d7c7970fc0b4fb7cd1811
lubuntum <lubuntum@gmail.com>
2024-09-21 09:34:03 +0700

    Полная работа с локальным пользователем

--- app/src/main/java/com/lubuntum/guesswhoapp/auth/AuthStorage.java
+++ app/src/main/java/com/lubuntum/guesswhoapp/auth/AuthStorage.java
@@ -10,18 +10,24 @@ public class AuthStorage {
     private static final String AUTH_STORAGE_NAME = "auth_storage";
     private static final String USER_LOCAL_NAME = "user_local";
     private static SharedPreferences preferences;
-
-    public static User getCurrentUser(Context context) {
+    public static void initSharedPreferences(Context context){
         if (preferences == null)
             preferences = context.getSharedPreferences(AUTH_STORAGE_NAME, Context.MODE_PRIVATE);
+    }
+
+    public static User getCurrentUser(Context context) {
+        initSharedPreferences(context);
         Gson gson = new Gson();
         return gson.fromJson(preferences.getString(USER_LOCAL_NAME, null), User.class);
     }
 
     public static void saveCurrentUser(Context context, User currentUser) {
-        if (preferences == null)
-            preferences = context.getSharedPreferences(AUTH_STORAGE_NAME, Context.MODE_PRIVATE);
+        initSharedPreferences(context);
         Gson gson = new Gson();
         preferences.edit().putString(USER_LOCAL_NAME, gson.toJson(currentUser)).apply();
+    }
+    public static boolean isUserExists(Context context){
+        initSharedPreferences(context);
+        return preferences.contains(USER_LOCAL_NAME);
     }
 }

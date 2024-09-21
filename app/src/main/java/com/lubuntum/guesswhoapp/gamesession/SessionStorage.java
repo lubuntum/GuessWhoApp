0a08ef22faa2232fcbf25aaa2531abb525a5b3fd
lubuntum <lubuntum@gmail.com>
2024-09-21 09:33:03 +0700

    Удалил ненужное хранилище

--- app/src/main/java/com/lubuntum/guesswhoapp/gamesession/SessionStorage.java
+++
@@ -1,7 +0,0 @@
-package com.lubuntum.guesswhoapp.gamesession;
-/*
-* Класс для хранение (локально) данных о текущем пользователе
-* */
-public class SessionStorage {
-    public static final String CURRENT_USER = "user_data";
-}
